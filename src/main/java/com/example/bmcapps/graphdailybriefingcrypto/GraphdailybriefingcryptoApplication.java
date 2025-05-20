d2cf1ce5d125d4cd44a4782831d9a395ef40dcfe
Brandon Campbell <brandon.camp.bell@outlook.com>
2025-05-20 18:18:21 -0500

    first commit

---
+++ src/main/java/com/example/bmcapps/graphdailybriefingcrypto/GraphdailybriefingcryptoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bmcapps.graphdailybriefingcrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphdailybriefingcryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphdailybriefingcryptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bmcapps/graphdailybriefingcrypto/GraphdailybriefingcryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bmcapps.graphdailybriefingcrypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphdailybriefingcryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
